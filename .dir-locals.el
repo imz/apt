76859bb47f5d0707f213ca690a046290703dec63
Ivan Zakharyaschev <imz@altlinux.org>
2020-08-20 16:43:40 +0300

    improve Emacs customization

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,10 +1,9 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
-
-((c++-mode
+((nil
+  (c-file-style . "bsd") ;; for indenting braces after "if", "for" etc.
   (c-basic-offset . 3))
 
- ;; For switching in .h headers:
+ ;; For switching the mode in .h headers:
  (c-mode
-  (mode . c++)
-  (c-basic-offset . 3)))
+  (mode . c++)))
