imz/apt

commit baba39c47f893f41653c46ac06c61b6a6a08f011

make it easier to output the cacheiterators in debug-messages (in depcache.cc)
ToDbgStr(Pkg).c_str() is equivalent to the old Pkg.Name(); I rewrite it
just to demonstrate how to use the new function.

It'll be more useful in the upcoming changes concerning more detailed
debugging messages.

apt-pkg/cacheiterators.h
@@ -363,4 +363,42 @@ inline pkgCache::DepIterator pkgCache::VerIterator::DependsList() const
 inline pkgCache::VerFileIterator pkgCache::VerIterator::FileList() const
        {return VerFileIterator(*Owner,Owner->VerFileP + Ver->FileList);}
 
+// Helpers for debugging
+// Often when outputting the info, we need to output multiple strings
+// joined together. (Previously, we did this with complex formats for printf.)
+// That's not convenient to do with a function: to return several strings.
+// Therefore we join them into a single std::string.
+
+inline std::string ToDbgStr(const pkgCache::PkgIterator &P)
+{
+   if (P.end())
+      return "(END)";
+   return
+      P.Name(); // NULL would be unexpected here, don't care
+}
+
+inline std::string ToDbgStr(const pkgCache::VerIterator &V)
+{
+   if (V.end())
+      return "(END)";
+   return
+      ToDbgStr(V.ParentPkg())
+      + " "
+      + V.VerStr(); // NULL would be unexpected here, don't care
+}
+
+inline std::string ToDbgStr(const pkgCache::DepIterator &D)
+{
+   if (D.end())
+      return "(END)";
+   return
+      std::string(D.DepType())
+      + ": "
+      + ToDbgStr(D.TargetPkg())
+      + " "
+      + D.CompType()
+      + " "
+      + (D.TargetVer() ?: "(NULL)");
+}
+
 #endif

apt-pkg/depcache.cc
@@ -863,7 +863,7 @@ void pkgDepCache::MarkInstallRec(const PkgIterator &Pkg,
 #define DEBUG_THIS(fmt, ...) DEBUG_MI(0, fmt, __VA_ARGS__)
 #define DEBUG_NEXT(fmt, ...) DEBUG_MI(1, fmt, __VA_ARGS__)
 
-   DEBUG_THIS("mark %s", Pkg.Name());
+   DEBUG_THIS("mark %s", ToDbgStr(Pkg).c_str());
 
    StateCache &P = PkgState[Pkg->ID];
    bool AddMarkAgain = false;
@@ -943,7 +943,7 @@ void pkgDepCache::MarkInstallRec(const PkgIterator &Pkg,
 	    }
 	    // In restricted mode, skip ambiguous dependencies.
 	    if (Restricted && CanSelect > 1) {
-	       DEBUG_NEXT("target %s AMBI", P.Name());
+	       DEBUG_NEXT("target %s AMBI", ToDbgStr(P).c_str());
 	       AddMarkAgain = true;
 	       // For now, skipping resolving this unsatisfied dep.
 	       continue; // to the next dep that might need satisfying
@@ -952,12 +952,12 @@ void pkgDepCache::MarkInstallRec(const PkgIterator &Pkg,
 
 	 if (InstPkg.end() == true)
 	 {
-            DEBUG_NEXT("target %s NONE", P.Name());
+            DEBUG_NEXT("target %s NONE", ToDbgStr(P).c_str());
             // Skipping resolving this unsatisfied dep.
             continue; // to the next dep that might need satisfying
 	 }
 
-	 DEBUG_NEXT("target %s SELECTED", P.Name());
+	 DEBUG_NEXT("target %s SELECTED", ToDbgStr(P).c_str());
          // Recursion is always restricted
          MarkInstallRec(InstPkg,/*Restricted*/true,MarkAgain,Depth+1,DebugStr);
       }
@@ -971,7 +971,7 @@ void pkgDepCache::MarkInstallRec(const PkgIterator &Pkg,
 	 {
 	    VerIterator const Ver(*this,*I);
 	    PkgIterator const Pkg = Ver.ParentPkg();
-	    DEBUG_NEXT("delete %s", Pkg.Name());
+	    DEBUG_NEXT("delete %s", ToDbgStr(Pkg).c_str());
 	    MarkDelete(Pkg);
 	    MarkAuto(Pkg, getMarkAuto(Pkg));
 	 }

commit bb1f1c535a28e9f06ef3f1a6eda9c824d76e7083

ToDbgStr(): fix so that DepType is output untranslated
Fixes: baba39c47 make it easier to output the cacheiterators in debug-messages (in depcache.cc)

apt-pkg/cacheiterators.h
@@ -410,7 +410,7 @@ inline std::string ToDbgStr(const pkgCache::DepIterator &D)
    if (D.end())
       return "(END)";
    return
-      std::string(D.DepType())
+      std::string(D.DepTypeC())
       + ": "
       + ToDbgStr(D.TargetPkg())
       + " "