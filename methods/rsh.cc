d12107ee66354a99e2b4bb7da13e750d440ef1d3
Alexey Sheplyakov <asheplyakov@basealt.ru>
2023-05-11 05:26:45 +0400

    methods: avoid compilation error with GCC 13
    http.cc:1014:18: error: ignoring return value of 'const _CharT* std::__cxx11::basic_string<_CharT, _Traits, _Alloc>::c_str() const [with _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]', declared with attribute 'nodiscard' [-Werror=unused-result]
     1014 |    FailFile.c_str();   // Make sure we dont do a malloc in the signal handler
          |    ~~~~~~~~~~~~~~^~
    In file included from /usr/include/c++/13/string:54,
                     from ../include/apt-pkg/fileutl.h:23,
                     from http.cc:29:
    /usr/include/c++/13/bits/basic_string.h:2594:7: note: declared here
     2594 |       c_str() const _GLIBCXX_NOEXCEPT
          |       ^~~~~
    cc1plus: all warnings being treated as errors
    make[1]: *** [Makefile:622: http.o] Error 1
    
    Similarly in ftp.cc and rsh.cc

--- methods/rsh.cc
+++ methods/rsh.cc
@@ -475,7 +475,7 @@ bool RSHMethod::Fetch(FetchItem *Itm)
       URIStart(Res);
 
       FailFile = Itm->DestFile;
-      FailFile.c_str();   // Make sure we dont do a malloc in the signal handler
+      (void)FailFile.c_str();   // Make sure we dont do a malloc in the signal handler
       FailFd = Fd.Fd();
 
       bool Missing;
