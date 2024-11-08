4c2c2fc0a56ba3d305d1dfeb77788352f62b0988
Ivan A. Melnikov <iv@altlinux.org>
2024-11-08 19:34:51 +0400

    Fix ShowEssential output
    Normally, packages in List are separated by a space; but when we
    are called from ShowEssential, List has additional information,
    and additional spaces. In this case, List cannot be reliably
    split into packages, so we should not use columnar output. This
    case can be detected by checking if VersionsList is empty.
    
    This seems to be the least invasive fix for ALT#51546

--- apt-pkg/cachefile.cc
+++ apt-pkg/cachefile.cc
@@ -520,7 +520,14 @@ bool ShowList(std::ostream &out, const std::string &Title, std::string List, con
    }
 
    bool const ShowVersions = _config->FindB("APT::Get::Show-Versions", false);
-   bool const ListColumns = _config->FindB("APT::Get::List-Columns", true);
+
+   // Normally, packages in List are separated by a space; but when we
+   // are called from ShowEssential, List has additional information,
+   // and additional spaces. In this case, List cannot be reliably
+   // split into packages, so we should not use columnar output. This
+   // case can be detected by checking if VersionsList is empty.
+   bool const ListColumns = _config->FindB("APT::Get::List-Columns", true)
+      && !VersionsList.empty();
 
    out << Title << std::endl;
    if (ListColumns && !ShowVersions)
