f9c1c906194f57b0073ba7578e7c29ea2eee45ef
Ivan Zakharyaschev <imz@altlinux.org>
2025-04-24 14:16:41 +0300

    connect-debug.h: comment about the choice of this for debugging and replaying

--- methods/connect-debug.h
+++ methods/connect-debug.h
@@ -4,6 +4,22 @@
    Wrapping of MethodFd (for Connect) and its debugging by wrapping
 
    ##################################################################### */
+
+/*
+A wrapper class is a more convenient and appropriate tool for
+debugging connections in APT than an external utility like strace
+because there can be several layers of connections, all abstracted by
+MethodFd, e.g., inside TLS, TLS inside TLS to proxy, etc.
+
+The kind of bugs that depend on the amound of bytes read() etc. happen
+randomly and often rarely. (It's difficult to catch the same one or
+somehow reproduce it deterministically.)
+
+TODO: One approach to reproduce them might be to use a wrapper class
+around MethodFd (like this wrapper for debugging and just outputting
+the operations), which would inject the Read() responses replaying the
+saved log from DebugMethodFd.
+*/
 									/*}}}*/
 #ifndef CONNECT_DEBUG_H
 #define CONNECT_DEBUG_H
