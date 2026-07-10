imz/apt

commit c27ced7a96490b2729f02ad06854fda7ac0abc27

Merging apt-rpm 0.5.5cnc1 over upstream APT in development trunk.

apt-pkg/repository.h
@@ -1,0 +1,57 @@
+// CNC:2002-07-03
+
+#ifndef PKGLIB_REPOSITORY_H
+#define PKBLIB_REPOSITORY_H
+
+#ifdef __GNUG__
+#pragma interface "apt-pkg/repository.h"
+#endif
+
+#include <string>
+#include <map>
+
+#include <apt-pkg/sourcelist.h>
+
+using std::map;
+
+class pkgRepository
+{
+   protected:
+   
+
+   struct Checksum {
+      unsigned long Size;
+      string MD5;
+      string SHA1;
+   };
+   
+   map<string,Checksum> IndexChecksums; // path -> checksum data   
+
+   bool GotRelease;
+   
+   public:   
+
+   string URI;
+   string Dist;
+   const pkgSourceList::Vendor *Vendor;
+   string RootURI;
+
+   bool Acquire;
+   
+   bool ParseRelease(string File);
+   bool HasRelease() const { return GotRelease; }
+
+   bool IsAuthenticated() const { return Vendor != NULL; };
+   bool FindChecksums(string URI,unsigned long &Size, string &MD5);
+   
+   pkgRepository(string URI,string Dist, const pkgSourceList::Vendor *Vendor,
+		 string RootURI)
+      : GotRelease(0), URI(URI), Dist(Dist), Vendor(Vendor), RootURI(RootURI),
+   	Acquire(1)
+      {};
+
+};
+
+#endif
+
+// vim:sts=3:sw=3

commit cb958155359d2a795fe77363b997041f021b6c52

apt-pkg/repository.h: fix typo in a header guard found by gcc 15 (-Werror=header-guard)
The typo was introduced in:

c27ced7a9 (Merging apt-rpm 0.5.5cnc1 over upstream APT in development trunk., 2003-02-03)

Recent GCC warns on this, which is treated as error:

In file included from acquire-item.cc:25:
../include/apt-pkg/repository.h:3: error: header guard 'PKGLIB_REPOSITORY_H' followed by '#define' of a different macro [-Werror=header-guard]
    3 | #ifndef PKGLIB_REPOSITORY_H
../include/apt-pkg/repository.h:4: note: 'PKBLIB_REPOSITORY_H' is defined here; did you mean 'PKGLIB_REPOSITORY_H'?
    4 | #define PKBLIB_REPOSITORY_H
cc1plus: all warnings being treated as errors

apt-pkg/repository.h
@@ -1,7 +1,7 @@
 // CNC:2002-07-03
 
 #ifndef PKGLIB_REPOSITORY_H
-#define PKBLIB_REPOSITORY_H
+#define PKGLIB_REPOSITORY_H
 
 #include <string>
 #include <map>